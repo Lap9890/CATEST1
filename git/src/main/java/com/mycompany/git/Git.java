884e082779930e5e741e7d01cd3148c9d5bac235
Lap9890 <lapicaf@gmail.com>
2026-03-14 17:57:14 +0000

    test

--- git/src/main/java/com/mycompany/git/Git.java
+++ git/src/main/java/com/mycompany/git/Git.java
@@ -13,5 +13,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Test");
+        System.out.println("Ok");
     }
 }
